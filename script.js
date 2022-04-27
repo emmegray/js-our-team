4ff93520a516958bbc9498f5914584a616753f33
emmegray <lunaienuso@libero.it>
2022-04-27 15:34:32 +0200

    Added Json member printed in page

--- script.js
+++ script.js
@@ -8,3 +8,48 @@ BONUS:
 Utilizzare gli input presenti nella pagina per permettere all'utente di aggiungere nuovi membri del team
 Cliccando sul pulsante "add" viene creato un nuovo oggetto che viene inserito nell'array iniziale e vie stampata una nuova card con tutte le info inserite dall'utente
 */
+
+const membriTeam = [
+    {
+        nome:'Angela Caroll',
+        ruolo:'Chief Editor',
+        foto:'img/angela-caroll-chief-editor.jpg'
+    },
+    {
+        nome:'Angela Lopez',
+        ruolo:'Social media manager',
+        foto:'img/angela-lopez-social-media-manager.jpg'
+    },
+    {
+        nome:'Barbara Ramos',
+        ruolo:'Graphic designer',
+        foto:'img/barbara-ramos-graphic-designer.jpg'
+    },
+    {
+        nome:'Scott Estrada',
+        ruolo:'Developer',
+        foto:'img/scott-estrada-developer.jpg'
+    },
+]
+
+const cardEsempio = document.querySelector(".team-card")
+const containerTeam = document.querySelector(".team-container")
+
+for (let index = 0; index < membriTeam.length; index++) {
+    const membro = membriTeam[index];
+    const nuovaCard = cardEsempio.cloneNode(true);
+
+    const img = nuovaCard.querySelector("img")
+    img.alt = membro.nome
+    img.src = membro.foto
+
+    const nomeMembro = nuovaCard.querySelector("h3")
+    nomeMembro.textContent = membro.nome
+
+    const ruoloMembro = nuovaCard.querySelector("p")
+    ruoloMembro.textContent = membro.ruolo
+
+    containerTeam.appendChild(nuovaCard)
+}
+
+cardEsempio.remove()
