447b5558cae1861c334f0bbe59b98664153b0af4
emmegray <lunaienuso@libero.it>
2022-04-27 15:10:57 +0200

    Added index, style and script

---
+++ script.js
@@ -0,0 +1,10 @@
+/* Our Team
+1. Definire un array dei membri del team
+   Ogni membro ha le informazioni necessarie per stampare la relativa card
+   (Nome, Ruolo e Foto)
+2. Una volta definita la struttura dati stampare dinamicamente una card per ogni membro del team
+
+BONUS:
+Utilizzare gli input presenti nella pagina per permettere all'utente di aggiungere nuovi membri del team
+Cliccando sul pulsante "add" viene creato un nuovo oggetto che viene inserito nell'array iniziale e vie stampata una nuova card con tutte le info inserite dall'utente
+*/
